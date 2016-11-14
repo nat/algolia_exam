ac19294de46f72a1623952a62aeca2606d38408a
N Wharton <wharton.n@gmail.com>
2016-11-14 11:33:33 +0100

    fixed seconds display, started implementation of facets and pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,13 @@ $(function () {
 	var INDEX_NAME = 'poc_restaurants';
 	var PARAMS = {
   		hitsPerPage: 3,
-  		maxValuesPerFacet: 8,
-  		index: INDEX_NAME
+  		maxValuesPerFacet: 7,
+  		index: INDEX_NAME,
+  		facets: ['food_type']
 	};
+	var FACETS_ORDER_OF_DISPLAY = ['food_type'];
+	var FACETS_LABELS = {food_type: 'Cuisine / Food Type'};
+
 	// Client + Helper initialization
 	var algolia = algoliasearch(APPLICATION_ID, SEARCH_ONLY_API_KEY);
 	var algoliaHelper = algoliasearchHelper(algolia, INDEX_NAME, PARAMS);
@@ -38,10 +42,69 @@ $(function () {
 	// Search results
 	algoliaHelper.on('result', function(content, state) {
   		renderHits(content);
+		renderStats(content);
+		renderFacets(content, state);
+		renderPagination(content);
 	});
 
 	function renderHits(content) {
 		$hits.html(hitTemplate.render(content));
 	}
+
+	function renderStats(content) {
+		var stats = {
+			nbHits: content.nbHits,
+			nbHits_plural: content.nbHits !== 1,
+			processingTimeSeconds: content.processingTimeMS / 1000
+		};
+		$stats.html(statsTemplate.render(stats));
+	}
+
+	function renderFacets(content, state) {
+		var facetsHtml = '';
+		var facetName = 'food_type';
+		var facetResult = content.getFacetByName(facetName);
+		var facetContent = {};
+		if (facetResult) {
+			facetContent = {
+				facet: facetName,
+				title: FACETS_LABELS[facetName],
+				values: content.getFacetValues(facetName, {sortBy: ['isRefined:desc', 'count:desc']}),
+				disjunctive: $.inArray(facetName, PARAMS.disjunctiveFacets) !== -1
+			};
+			facetsHtml += facetTemplate.render(facetContent);
+		}
+		$facets.html(facetsHtml);
+	}
+
+	function renderPagination(content) {
+		// console.log(content);
+		// console.log(content.page);
+		// console.log(content.nbPages);
+		var pages = [];
+		if (content.page > 3) {
+			pages.push({current: false, number: 1});
+			pages.push({current: false, number: '...', disabled: true});
+		}
+		for (var p = content.page - 3; p < content.page + 3; ++p) {
+			if (p < 0 || p >= content.nbPages) continue;
+			pages.push({current: content.page === p, number: p + 1});
+		}
+		if (content.page + 3 < content.nbPages) {
+			pages.push({current: false, number: '...', disabled: true});
+			pages.push({current: false, number: content.nbPages});
+		}
+		var pagination = {
+			pages: pages,
+			prev_page: content.page > 0 ? content.page : false,
+			next_page: content.page + 1 < content.nbPages ? content.page + 2 : false
+		};
+		$pagination.html(paginationTemplate.render(pagination));
+	}
+	$(document).on('click', '.go-to-page', function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop: 0}, '500', 'swing');
+		algoliaHelper.setCurrentPage(+$(this).data('page') - 1).search();
+	});
 
 });
